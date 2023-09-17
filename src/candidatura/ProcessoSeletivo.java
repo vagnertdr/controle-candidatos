21fbd96963d83377269ac5d6cbecec44f463d333
Vagner Rocha <vagnerrochab2@gmail.com>
2023-09-17 11:13:15 -0300

    Initial commit

---
+++ src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,22 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+
+        analisarCandidato(1900.0);
+        analisarCandidato(2050.0);
+        analisarCandidato(2000.0);
+
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+}
